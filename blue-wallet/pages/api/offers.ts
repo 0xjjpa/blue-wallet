0456390639b07232e57f1a67f20b3629aed29509
jjperezaguinaga <me@jjperezaguinaga.com>
2022-08-07 03:22:55 +0000

    feature: Completed offers endpoint

--- blue-wallet/pages/api/offers.ts
+++ blue-wallet/pages/api/offers.ts
@@ -7,21 +7,36 @@ import { NextApiResponse, NextApiRequest } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{}>
+  res: NextApiResponse<{ offers?: number[], err?: string }>
 ) {
   const provider = String(process.env.ETH_PROVIDER);
-  const { body: { collectionAddress, tokenId } } = req;
+  const { query: { address, tokenId } } = req;
   try {
+    const collectionAddress = address instanceof Array ? address[0] : address
+    const _tokenId = tokenId instanceof Array ? tokenId[0] : tokenId
     if (!collectionAddress) return res.status(501).json({ err: 'No collectionAddress provided' });
-    if (!tokenId) return res.status(501).json({ err: 'No tokenId provided' });
+    if (!_tokenId) return res.status(501).json({ err: 'No tokenId provided' });
 
     const readProvider = new providers.JsonRpcProvider(provider);
     const offerModuleContract = OffersV1__factory.connect(rinkebyZoraAddresses.OffersV1, readProvider);
 
-    const offers = await offerModuleContract.offersForNFT(getAddress(collectionAddress), tokenId, 0);
-
+    let hasValidOffers = true;
+    const offers = []
+    let index = 0
+    while (hasValidOffers) {
+      try {
+        const offer = await offerModuleContract.offersForNFT(getAddress(collectionAddress), _tokenId, index);
+        offers.push(offer.toNumber())
+        index++;
+      } catch (err: any) {
+        if (err.code == 'CALL_EXCEPTION') {
+          hasValidOffers = false;
+        }
+      }
+    }
     return res.status(200).json({ offers })
   } catch (err: any) {
     if (err.code == 'INVALID_ARGUMENT') return res.status(422).json({ err: `Invalid collection address` })
+    return res.status(501).json({ err })
   }
 }
